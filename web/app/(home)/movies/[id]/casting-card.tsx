9e0406f0d3d2f5c825ed1be8c7add2934a2791d0
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2024-06-05 23:04:48 +0200

    UI Improvements

--- web/app/(home)/movies/[id]/casting-card.tsx
+++ web/app/(home)/movies/[id]/casting-card.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
+import { Cast } from "@/services/movie";
 
-export function CastingCard({posterUrl, name, role}: {posterUrl: string, name: string, role: string}) {
+export function CastingCard({cast}: {cast: Cast}) {
     return (
         <>
-            <Card className="flex flex-row gap-4 p-4">
-                <Image src={posterUrl} alt={name} className="w-20 h-20 rounded-lg" width="20" height="20"/>
+            <Card className="flex flex-row gap-4 p-4 content-center items-center">
+                <Image src={cast.profileUrl} alt={cast.name} className="w-20 h-20 rounded-lg" width="40" height="40"/>
                 <div className="flex flex-col justify-center">
-                    <h3 className="text-lg font-semibold">{name}</h3>
-                    <p className="text-sm text-gray-500">{role}</p>
+                    <h3 className="text-lg font-semibold">{cast.name}</h3>
+                    <p className="text-sm text-gray-500">{cast.role}</p>
                 </div>
             </Card>
         </>
--- web/app/(home)/movies/[id]/vote-slider.tsx
+++ web/app/(home)/movies/[id]/vote-slider.tsx
@@ -1,28 +1,34 @@
+'use client'
+
 import React from 'react';
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
-const VoteSlider = () => {
-  const [value, setValue] = useState(5);
+const VoteSlider = ({userVote}: {userVote: number}) => {
+  const [value, setValue] = useState(userVote);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(parseInt(e.target.value));
   }
 
   return (
-    <div className="flex flex-row items-center space-x-4 h-full content-center justify-between w-[250px] mx-4">
-      <input
-        type="range"
-        min="1"
-        max="10"
-        step="1"
-        value={value}
-        onChange={handleChange}
-        className="w-full h-2 bg-gray-200 rounded-full appearance-none cursor-pointer dark:bg-black"
-      />
-      <div className="m-0 text-lg font-medium text-black text-center items-center content-center">
-        {value}
+      <div className="my-3 flex flex-row items-center content-center">
+          <div className="flex flex-row items-center space-x-4 h-full content-center justify-between w-[250px] mx-4">
+              <div className="m-0 text-lg font-medium text-black text-center items-center content-center">
+                  {value}
+              </div>
+              <input
+                  type="range"
+                  min="1"
+                  max="10"
+                  step="1"
+                  value={value}
+                  onChange={handleChange}
+                  className="w-full h-2 bg-gray-200 rounded-full appearance-none cursor-pointer dark:bg-black"
+              />
+          </div>
+          <Button>Vote</Button>
       </div>
-    </div>
   );
 };
 
