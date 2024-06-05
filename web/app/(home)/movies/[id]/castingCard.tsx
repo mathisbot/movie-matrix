a14475bfe15eadf2acb00a16fdcf5ee780dd9cb7
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2024-06-05 16:51:45 +0200

    Improved SingleMovie

---
+++ web/app/(home)/movies/[id]/castingCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+
+export function CastingCard({posterUrl, name, role}: {posterUrl: string, name: string, role: string}) {
+    return (
+        <>
+            <Card className="flex flex-row gap-4 p-4">
+                <Image src={posterUrl} alt={name} className="w-20 h-20 rounded-lg" width="20" heigh="20"/>
+                <div className="flex flex-col justify-center">
+                    <h3 className="text-lg font-semibold">{name}</h3>
+                    <p className="text-sm text-gray-500">{role}</p>
+                </div>
+            </Card>
+        </>
+    );
+}
--- web/app/(home)/movies/[id]/page.tsx
+++ web/app/(home)/movies/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getAuthMetadata, movieServiceClient } from "@/lib/grpc";
 import Image from "next/image";
 import { Card } from "@/components/ui/card";
+import { CastingCard } from "./castingCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function convertRuntime(runtime: number) {
   const hours = Math.floor(runtime / 60);
@@ -30,7 +32,7 @@ export default async function MoviePage({
 
   return (
     <div
-      className="p-5"
+      className="p-7"
       style={
         movie.backdropUrl
           ? {
@@ -41,30 +43,44 @@ export default async function MoviePage({
           : {}
       }
     >
-      <div className="flex flex-row item-center justify-center text-white align-center">
+      {/* <div className="flex flex-row item-center justify-center text-white align-center">
         <h1 className="text-4xl font-bold mb-4">{movie.title}</h1>
-      </div>
-      <div className="flex rounded-xl flex-row items-center bg-white p-4 justify-between">
+      </div> */}
+      <div className="flex rounded-xl flex-row bg-white p-5 justify-between items-center align-middle opacity-95">
         {movie.posterUrl && (
           <Image
             src={movie.posterUrl}
             alt={movie.title}
-            className="mb-4 mr-2 rounded-xl"
+            className="mb-4 mr-2 rounded-xl opacity-100"
             width="350"
             height="525"
           />
         )}
-        <Card className="ml-2 p-4 h-full">
+        <Card className="ml-2 p-4 h-full opacity-100">
           <h2 className="mb-4 text-xl">{movie.title}</h2>
           <p className="mb-4">⭐ Rating: {movie.voteAverage.toFixed(1)} /10</p>
           <p className="mb-4">🕰️ Duration: {convertRuntime(movie.runtime)}</p>
-          <p className="mb-4">{movie.overview}</p>
-          <p className="mb-4 text-lg">🙋 Casting:</p>
-          <ul>
-            {movie.casting.map((actor, index) => (
-              <li key={index}>{JSON.stringify(actor)}</li>
-            ))}
-          </ul>
+          <Tabs defaultValue="synopsis" className="w-full">
+            <TabsList>
+              <TabsTrigger value="synopsis">Synopsis</TabsTrigger>
+              <TabsTrigger value="casting">🙋 Casting</TabsTrigger>
+            </TabsList>
+            <TabsContent value="synopsis">
+              <Card className="w-full p-3 bg-gray-100">
+                <p className="mb-4 text-justify">{movie.overview}</p>
+              </Card>
+            </TabsContent>
+            <TabsContent value="casting" className="w-full">
+              <Card className="w-full p-3 bg-gray-100">
+                <ul className="grid grid-flow-row w-full">
+                  {movie.casting.map((actor, index) => (
+                    <li key={index}>{JSON.stringify(actor)}</li>
+                  ))}
+                </ul>
+                <CastingCard posterUrl="" name="" role="" />
+              </Card>
+            </TabsContent>
+          </Tabs>
         </Card>
       </div>
     </div>
