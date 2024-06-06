c8fb7452a66aadbb2b3d9c000ee463c258c400a8
Emile Jaffrain <emile.jaffrain@gmail.com>
2024-06-06 15:45:26 +0200

    [web] better single movie component

--- web/app/(home)/movies/[id]/single-movie.tsx
+++ web/app/(home)/movies/[id]/single-movie.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useEffect } from "react";
 import Image from "next/image";
@@ -17,11 +17,21 @@ function convertRuntime(runtime: number) {
 
 function formatDate(dateString: string) {
   const months = [
-    "January", "February", "March", "April", "May", "June", 
-    "July", "August", "September", "October", "November", "December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
-  const [year, month, day] = dateString.split('-');
+  const [year, month, day] = dateString.split("-");
   const monthName = months[parseInt(month, 10) - 1];
 
   return `${parseInt(day, 10)} ${monthName} ${year}`;
@@ -41,128 +51,131 @@ export default function SingleMovie({ movie }: { movie: Movie }) {
   }, []);
 
   return (
-      <>
-          <div className="relative h-screen overflow-hidden">
-              <div
-                  id="parallax"
-                  className="absolute top-0 left-0 w-full h-full"
-                  style={
-                      movie.backdropUrl
-                          ? {
-                                backgroundImage: `url(${movie.backdropUrl})`,
-                                backgroundSize: "cover",
-                                backgroundAttachment: "fixed",
-                            }
-                          : {}
-                  }
+    <>
+      <div className="relative h-screen overflow-hidden">
+        <div
+          id="parallax"
+          className="absolute top-0 left-0 w-full h-full"
+          style={
+            movie.backdropUrl
+              ? {
+                  backgroundImage: `url(${movie.backdropUrl})`,
+                  backgroundSize: "cover",
+                  backgroundAttachment: "fixed",
+                }
+              : {}
+          }
+        >
+          <div className="relative z-10 h-full flex items-end">
+            <div className="relative z-10 w-screen h-[200px]">
+              <div className="absolute z-15 inset-0 bg-black opacity-40 w-full h-full"></div>
+              <div className="relative mb-10">
+                <h2 className="text-7xl text-white px-10 mt-4">
+                  <span className="relative z-10">{movie.title}</span>
+                </h2>
+                <p className="relative px-10 text-white text-lg">
+                  Rating: {movie.voteAverage.toFixed(1)} /10
+                </p>
+                <p className="relative px-10 text-white text-lg">
+                  Duration: {convertRuntime(movie.runtime)}
+                </p>
+              </div>
+              <Link
+                id="details-link"
+                href="#details-link"
+                className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-black text-white p-4 rounded-full shadow-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
               >
-                  <div className="relative z-10 h-full flex items-end">
-                      <div className="relative z-10 w-screen h-[200px]">
-                          <div className="absolute z-15 inset-0 bg-black opacity-40 w-full h-full"></div>
-                          <div className="relative mb-10">
-                              <h2 className="text-7xl text-white px-10 mt-4">
-                                  <span className="relative z-10">
-                                      {movie.title}
-                                  </span>
-                              </h2>
-                              <p className="relative px-10 text-white text-lg">
-                                  Rating: {movie.voteAverage.toFixed(1)} /10
-                              </p>
-                              <p className="relative px-10 text-white text-lg">
-                                  Duration: {convertRuntime(movie.runtime)}
-                              </p>
-                          </div>
-                        <Link
-                            id="details-link"
-                            href="#details-link"
-                            className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-black text-white p-4 rounded-full shadow-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
-                            >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-                            </svg>
-                        </Link>
-                      </div>
-                  </div>
-              </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </Link>
+            </div>
           </div>
-          <div id="details" className="relative z-20 bg-gray-100">
-              <div className="flex rounded-xl flex-row p-5 justify-between items-center align-middle">
-                  {movie.posterUrl && (
-                      <Image
-                          src={movie.posterUrl}
-                          alt={movie.title}
-                          className="mb-4 rounded-xl"
-                          width="350"
-                          height="525"
-                      />
-                  )}
-                  <div className="ml-2 p-4 h-full w-full">
-                      <h2 className="mb-4 text-3xl">{movie.title}</h2>
-                      <div className="mb-4 ml-8">
-                          <p>Already watched ? Tell us how you liked it:</p>
-                          <VoteSlider userVote={movie.userVote? movie.userVote : 5} movieId={movie.id}></VoteSlider>
-                      </div>
-                      <p className="mb-4 text-lg">
-                          ⭐ {movie.voteAverage.toFixed(1)} / 10
-                      </p>
-                      <p className="mb-4 text-lg">
-                          🕰️ {convertRuntime(movie.runtime)}
-                      </p>
-                      <p className="mb-4 text-lg">
-                          📅 {formatDate(movie.releaseDate)}
-                      </p>
-                      <p className="mb-4 text-lg">
-                          🎬 {movie.genres.join(", ")}
-                      </p>
-                      <Tabs defaultValue="synopsis">
-                          <TabsList>
-                              <TabsTrigger value="synopsis">
-                                  📖 Synopsis
-                              </TabsTrigger>
-                              <TabsTrigger value="casting">
-                                  🙋 Casting
-                              </TabsTrigger>
-                          </TabsList>
-                          <TabsContent value="synopsis" className="max-w-full">
-                              <Card className="w-full p-3 bg-white">
-                                  <p className="mb-4 text-justify">
-                                      {movie.overview}
-                                  </p>
-                              </Card>
-                          </TabsContent>
-                          <TabsContent value="casting">
-                              <Card className="w-full p-3 bg-white">
-                                  <ul className="flex flex-row flex-wrap w-full">
-                                      {movie.casting.map((cast, index) => (
-                                          <li key={index}>
-                                              <CastingCard cast={cast} />
-                                          </li>
-                                      ))}
-                                  </ul>
-                              </Card>
-                          </TabsContent>
-                      </Tabs>
-                  </div>
-              </div>
+        </div>
+      </div>
+      <div id="details" className="relative z-20 bg-gray-100">
+        <div className="flex rounded-xl flex-row p-5 justify-between align-middle">
+          <div>
+            {movie.posterUrl && (
+              <Image
+                src={movie.posterUrl}
+                alt={movie.title}
+                className="mb-4 rounded-xl sticky top-16"
+                width="350"
+                height="525"
+              />
+            )}
           </div>
-          <div id="details" className="relative z-20">
-          <div className="relative mb-10">
-                              <h2 className="text-4xl text-black px-12 mt-8 mb-8">
-                                  <span className="relative z-10">
-                                      You may also like:
-                                  </span>
-                              </h2>
-                              <Card className="w-full p-3 bg-white px-20">
-                                  <ul className="grid grid-flow-row w-full">
-                                      {movie.casting.map((cast, index) => (
-                                          <li key={index}>
-                                              <CastingCard cast={cast} />
-                                          </li>
-                                      ))}
-                                  </ul>
-                              </Card>
-                          </div>
+          <div className="ml-2 p-4 h-full w-full">
+            <h2 className="mb-4 text-3xl">{movie.title}</h2>
+            <div className="mb-4">
+              <p>Already watched ? Tell us how you liked it:</p>
+              <VoteSlider
+                userVote={movie.userVote ? movie.userVote : 5}
+                movieId={movie.id}
+              />
+            </div>
+            <p className="mb-4 text-lg">
+              ⭐ {movie.voteAverage.toFixed(1)} / 10
+            </p>
+            <p className="mb-4 text-lg">🕰️ {convertRuntime(movie.runtime)}</p>
+            <p className="mb-4 text-lg">📅 {formatDate(movie.releaseDate)}</p>
+            <p className="mb-4 text-lg">🎬 {movie.genres.join(", ")}</p>
+            <Tabs defaultValue="synopsis">
+              <TabsList>
+                <TabsTrigger value="synopsis">📖 Synopsis</TabsTrigger>
+                <TabsTrigger value="casting">🙋 Casting</TabsTrigger>
+              </TabsList>
+              <TabsContent value="synopsis" className="max-w-full">
+                <Card className="w-full p-3 ">
+                  <p className="mb-4 text-justify">{movie.overview}</p>
+                </Card>
+              </TabsContent>
+              <TabsContent value="casting">
+                <Card className="w-full p-3">
+                  <ul className="grid grid-cols-8">
+                    {movie.casting
+                      .filter((v) => v.profileUrl)
+                      .slice(0, 16)
+                      .map((cast, index) => (
+                        <li key={index}>
+                          <CastingCard cast={cast} />
+                        </li>
+                      ))}
+                  </ul>
+                </Card>
+              </TabsContent>
+            </Tabs>
           </div>
-      </>
+        </div>
+      </div>
+      <div id="details" className="relative z-20">
+        <div className="relative mb-10">
+          <h2 className="text-4xl text-black px-12 mt-8 mb-8">
+            <span className="relative z-10">You may also like:</span>
+          </h2>
+          <Card className="w-full p-3 bg-white px-20">
+            <ul className="grid grid-flow-row w-full">
+              {movie.casting.slice(0, 7).map((cast, index) => (
+                <li key={index}>
+                  <CastingCard cast={cast} />
+                </li>
+              ))}
+            </ul>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
--- web/app/(home)/movies/[id]/vote-slider.tsx
+++ web/app/(home)/movies/[id]/vote-slider.tsx
@@ -1,69 +1,73 @@
-'use client'
+"use client";
 
-import React from 'react';
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { useQuery } from '@tanstack/react-query';
-import { voteMovie } from '../actions';
-import { VoteMovieResponse } from '@/services/movie';
-import { Loader } from 'lucide-react';
+import React from "react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
+import { voteMovie } from "../actions";
+import { VoteMovieResponse } from "@/services/movie";
+import { Loader } from "lucide-react";
 
-const VoteSlider = ({userVote, movieId}: {userVote: number, movieId: number}) => {
-    const [value, setValue] = useState(userVote);
+const VoteSlider = ({
+  userVote,
+  movieId,
+}: {
+  userVote: number;
+  movieId: number;
+}) => {
+  const [value, setValue] = useState(userVote);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(parseInt(e.target.value));
-    }
+  const adjectiveMap: { [key: number]: string } = {
+    1: "Appalling",
+    2: "Horrible",
+    3: "Very Bad",
+    4: "Bad",
+    5: "Average",
+    6: "Fine",
+    7: "Good",
+    8: "Very good",
+    9: "Great",
+    10: "Masterpiece",
+  } as const;
 
-    const adjectiveMap = {
-        1: "Appalling",
-        2: "Horrible",
-        3: "Very Bad",
-        4: "Bad",
-        5: "Average",
-        6: "Fine",
-        7: "Good",
-        8: "Very good",
-        9: "Great",
-        10: "Masterpiece"
-      };
-    
-    const { data: vote, refetch: reVote, isFetching: isVoting, isFetched: isVoted } = useQuery({
-        queryKey: ["search-movies"],
-        queryFn: async () => (await voteMovie(movieId, value) as VoteMovieResponse),
-        enabled: false,
-    });
+  const {
+    refetch: reVote,
+    isFetching: isVoting,
+    isFetched: isVoted,
+  } = useQuery({
+    queryKey: ["search-movies"],
+    queryFn: async () => (await voteMovie(movieId, value)) as VoteMovieResponse,
+    enabled: false,
+  });
 
-    const handleClick = () => {
-        reVote();
-    }
-
-    return (
-        <div className="my-3 flex flex-row items-center content-center">
-            <div className="flex flex-row items-center space-x-4 h-full content-center justify-between w-[250px] mx-4">
-                <div className="m-0 text-lg font-medium text-black text-center items-center content-center">
-                    {value}
-                </div>
-                <input
-                    type="range"
-                    min="1"
-                    max="10"
-                    step="1"
-                    value={value}
-                    onChange={handleChange}
-                    className="w-full h-2 bg-gray-200 rounded-full appearance-none cursor-pointer dark:bg-black"
-                />
-                <p className="text-center">{adjectiveMap[value]}</p>
-            </div>
-            <Button onClick={handleClick}>
-                {isVoting ? (
-                    <Loader className="mx-3 size-5 animate-spin " />
-                ) : isVoted ? (
-                    "Voted!"
-                ) : "Vote"}
-            </Button>
+  return (
+    <div className="my-3 flex flex-row items-center content-center">
+      <div className="flex flex-row items-center space-x-4 h-full content-center justify-between mx-4">
+        <div className="m-0 text-lg font-medium text-black text-center items-center content-center">
+          {value}
         </div>
-    );
+        <input
+          type="range"
+          min="1"
+          max="10"
+          step="1"
+          value={value}
+          onChange={(event) => setValue(parseInt(event.target.value))}
+          className="w-full h-2 bg-gray-200 rounded-full appearance-none cursor-pointer dark:bg-black"
+        />
+        <p className="text-center w-32">{adjectiveMap[value]}</p>
+      </div>
+      <Button onClick={() => reVote()}>
+        {isVoting ? (
+          <Loader className="mx-3 size-5 animate-spin " />
+        ) : isVoted ? (
+          "Voted!"
+        ) : (
+          "Vote"
+        )}
+      </Button>
+    </div>
+  );
 };
 
 export default VoteSlider;
